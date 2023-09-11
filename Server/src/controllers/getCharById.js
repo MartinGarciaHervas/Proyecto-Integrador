dc5f74e4486eb57850b393ed8be650b54352fb1d
Martin Garcia Hervas <punighervas@gmail.com>
2023-09-11 16:53:35 -0300

    json

--- Server/src/controllers/getCharById.js
+++ Server/src/controllers/getCharById.js
@@ -23,13 +23,12 @@ async function getCharById (req, res){
     const {data} = await axios(`https://rickandmortyapi.com/api/character/${id}`);
 
         if(data.name){
-            res.status(200).send(data)
+            res.status(200).json(data)
         } else {
-           return res.status(404).send('Not Found')
+            res.status(404).send('Not Found')
         }
-    }catch{(error)=>{
+    } catch (error){
         res.status(500).send(error.message)
     }}
-};
 
 module.exports = getCharById;
