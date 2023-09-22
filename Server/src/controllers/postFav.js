e4e5bf9313823b5a417106495385a8db80583a6f
Martin Garcia Hervas <punighervas@gmail.com>
2023-09-21 21:58:22 -0300

    Register Ok

--- Server/src/controllers/postFav.js
+++ Server/src/controllers/postFav.js
@@ -4,13 +4,13 @@ const postFav = async (req, res) => {
     try {
         const { id, name, origin, status, image, species, gender } = req.body;
         if (id && name && origin && status && image && species && gender) {
-            await Favorite.findOrCreate({ where: { id:id, name:name, origin:origin, status:status, image:image, species:species, gender:gender }, defaults: { id, name, origin, status, image, species, gender } });
+            await Favorite.findOrCreate({ where: { id: id, name: name, origin: origin, status: status, image: image, species: species, gender: gender }, defaults: { id, name, origin, status, image, species, gender } });
             const favorites = await Favorite.findAll();
             return res.status(200).json(favorites)
         }
         return res.status(400).json('Faltan datos');
     } catch (error) {
-        res.status(500).json({error:error.message})
+        res.status(500).json({ error: error.message })
     }
 };
 
